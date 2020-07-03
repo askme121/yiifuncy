01498759098dd8f9b2fecb7162886ea17fa5210f
askme121 <askme-121@163.com>
2020-07-03 18:23:19 +0800

    ok

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,14 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2cms',
+            'username' => 'root',
+            'password' => 'goodjob',
+            'charset' => 'utf8',
+            'tablePrefix' => 't_',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
