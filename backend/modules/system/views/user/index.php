4fd93b384b3d54a4f0312d4a48429e06206380df
askme121 <askme-121@163.com>
2020-08-01 23:32:37 +0800

    ok

--- backend/modules/system/views/user/index.php
+++ backend/modules/system/views/user/index.php
@@ -8,13 +8,52 @@ use common\models\UserRank;
 LayuiAsset::register($this); 
 $this->registerJs($this->render('js/index.js'));
 ?>
+<style type="text/css">
+    .layui-input-inline{
+        max-width: 150px;
+    }
+    .nav-myself-ul{
+        width: 100px;
+        color: #666;
+        list-style: none;
+    }
+    .nav-myself-li{
+        float: left;
+        padding: 5px 8px;
+        position: relative;
+    }
+    .nav-myself-dl{
+        display: none;
+        position: absolute;
+        top:30px;
+        border: 1px solid #d2d2d2;
+        border-radius: 2px;
+        padding: 5px 15px;
+        box-shadow: 0 2px 4px rgba(0,0,0,.12);
+        z-index: 9999;
+        background: #d2d2d2;
+    }
+    .nav-myself-li:hover .nav-myself-dl{
+        display: block;
+    }
+    .nav-myself-li a{
+        color: #666;
+        text-decoration: none;
+    }
+    .nav-myself-dl dd{
+        margin: 5px auto;
+    }
+    .nav-myself-dl dd a{
+        color: #333;
+    }
+</style>
 <blockquote class="layui-elem-quote" style="font-size: 14px;">
 	<?php  echo $this->render('_search', ['model' => $searchModel]); ?>
 </blockquote>
 <div class="user-index layui-form news_list">
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
-		'options' => ['class' => 'grid-view','style'=>'overflow:auto', 'id' => 'grid'],
+		'options' => ['class' => 'grid-view','style'=>'overflow:auto; padding-bottom:90px', 'id' => 'grid'],
 		'tableOptions'=> ['class'=>'layui-table'],
 		'pager' => [
 			'options'=>['class'=>'layuipage pull-right'],
@@ -102,23 +141,32 @@ $this->registerJs($this->render('js/index.js'));
 					'width' => '10%',
 					'style'=> 'text-align: center;'
 				],
-				'template' =>'{view} {update} {activate} {delete}',
+				'template' =>'<ul class="nav-myself-ul">
+                             <li class="nav-myself-li">
+                                 {view}
+                                 <dl class="nav-myself-dl">
+                                     {update}
+                                     <dd>{delete}</dd>
+                                 </dl>
+                             </li>
+                             {activate}
+                          </ul>',
 				'buttons' => [
                     'view' => function ($url, $model, $key){
-						return Html::a('查看', Url::to(['view','id'=>$model->id]), ['class' => "layui-btn layui-btn-xs layui-default-view"]);
+						return Html::a('查看', Url::to(['view','id'=>$model->id]), ['class' => "layui-default-view"]);
                     },
                     'update' => function ($url, $model, $key) {
-						return Html::a('修改', Url::to(['update','id'=>$model->id]), ['class' => "layui-btn layui-btn-normal layui-btn-xs layui-default-update"]);
+                        return '<dd>'.Html::a('编辑', Url::to(['update','id'=>$model->id]), ['class' => "layui-default-update"]).'</dd>';
                     },
                     'activate' => function ($url, $model, $key) {
 						if($model->status==0){
-							return Html::a('启用', Url::to(['active','id'=>$model->id]), ['class' => "layui-btn layui-btn-xs layui-btn-normal layui-default-active"]);
+                            return '<li class="nav-myself-li">'.Html::a('启用', Url::to(['active','id'=>$model->id]), ['class' => "layui-default-active"]).'</li>';
 						}else{
-							return Html::a('禁用', Url::to(['inactive','id'=>$model->id]), ['class' => "layui-btn layui-btn-xs layui-btn-warm layui-default-inactive"]);
+                            return '<li class="nav-myself-li">'.Html::a('禁用', Url::to(['inactive','id'=>$model->id]), ['class' => "layui-default-active"]).'</li>';
 						}
                     },
 					'delete' => function ($url, $model, $key) {
-						return Html::a('删除', Url::to(['delete','id'=>$model->id]), ['class' => "layui-btn layui-btn-danger layui-btn-xs layui-default-delete"]);
+                        return Html::a('删除', Url::to(['delete','id'=>$model->id]), ['class' => "layui-default-delete"]);
 					}
 				]
 			],
