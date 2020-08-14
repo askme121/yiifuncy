7b9bc9ed786a6f25fbacdf26928588d078a861f3
askme121 <askme-121@163.com>
2020-08-14 15:03:33 +0800

    ok

--- console/controllers/ToolController.php
+++ console/controllers/ToolController.php
@@ -2,11 +2,24 @@
 
 namespace console\controllers;
 
+use common\models\Order;
 use yii;
 use yii\console\Controller;
 
 class ToolController extends Controller
 {
+    public function actionExpire()
+    {
+        $expire_day = Yii::$app->params['order.Expire'];
+        $list = Order::find()->where(['status'=>1])->andWhere(['<', 'created_at', time()-$expire_day*24*3600])->all();
+        if ($list){
+            foreach ($list as $item){
+                $item->status = 7;
+                $item->save();
+            }
+        }
+    }
+
     public function actionInit()
     {
         $str = '';
